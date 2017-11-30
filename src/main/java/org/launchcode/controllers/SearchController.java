1bd466402fae1b52d76fa770e407bcd0d61cdc3c
shapeshiftingpedro <sifibookworm@gmail.com>
2017-11-29 19:49:01 -0600

    Completed to-dos

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -22,6 +22,21 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value="results")
+    public String searchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        if (searchType.equals("all")){
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+            model.addAttribute("title", "Jobs with the term: " + searchTerm );
+            model.addAttribute("jobs", jobs);
+
+            return "search";
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with the " + ListController.columnChoices.get(searchType) + " value as " + searchTerm);
+            model.addAttribute("jobs", jobs);
+            return "search";
+        }
+    }
+    // TODO #1 - Create handler to process search request and display results- Complete
 
 }
